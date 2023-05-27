b539223509951deca04c00457de85ba1778cc7af
Gauri Patil <gaurip381@gmail.com>
2023-05-27 20:39:57 +0530

    changes

---
+++ getxapi/lib/views/homepage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: Icon(
+          Icons.arrow_back_ios,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'ShopX',
+                    style: TextStyle(
+                        fontFamily: 'avenir',
+                        fontSize: 32,
+                        fontWeight: FontWeight.w900),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.view_list_rounded),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.grid_view),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
